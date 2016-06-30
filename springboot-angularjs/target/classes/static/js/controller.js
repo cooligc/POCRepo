18e42c43fa2f9c07f7f3a71be0df61537ad3b4f9
Chaudhury <sitakanta.chaudhury@hpe.com>
2016-06-30 19:49:12 +0530

    Remove the unnecessery binaries

--- springboot-angularjs/target/classes/static/js/controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('usersController', function($scope) {
-    $scope.headingTitle = "User List";
-});
-
-app.controller('rolesController',function($scope,RoleService) {
-    $scope.headingTitle = "Roles List";
-    $scope.roles = RoleService.findRoles();
-    console.log(RoleService.findRoles());
-});
